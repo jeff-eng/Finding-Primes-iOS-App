0c2bf0ab57212d4bf7f6b435be5046aa2df7e0ad
Jeff Eng <jeff@jeffeng.me>
2016-07-03 17:49:30 -0700

    added in the logic from the playgrounds file and also modified the README file

--- Got Prime/Got Prime/ViewController.swift
+++ Got Prime/Got Prime/ViewController.swift
@@ -9,7 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var numberTextField: UITextField!
 
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func clearButtonPressed(sender: AnyObject) {
+        numberTextField.text = ""
+    }
+    
+    @IBAction func checkIfPrimeButton(sender: AnyObject) {
+        
+        let numberConvertedFromTextField: Int = Int(numberTextField.text!)!
+        
+        func isPrime(number: Int) -> Bool {
+            if number <= 1 {
+                return false
+            }
+            if number <= 3 {
+                return true
+            }
+            
+            var i = 2
+            
+            while i*i <= number {
+                if number % i == 0 {
+                    return false
+                }
+                
+                i = i + 1
+            }
+            
+            return true
+        }
+        
+        isPrime(numberConvertedFromTextField)
+        
+        if isPrime(numberConvertedFromTextField) == true {
+            resultLabel.text = "\(numberConvertedFromTextField) is a prime number!"
+        } else {
+            resultLabel.text = "\(numberConvertedFromTextField) is not a prime number."
+        }
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Finding-Primes-iOS-App
 
 This app finds prime numbers and is built using Xcode 7 and Swift 2.  The code was written and tested in Xcode playgrounds and then ported to the Xcode project.
+
+##Concepts Used/Practiced:
+* Xcode Auto Layout with constraints
+* Functions in Swift
+
+## Notes:
+* Apple values descriptive naming conventions.  They advise clarity and brevity, however clarity shouldn't be prioritized for the sake of brevity.
+* Apple also advises to not abbreviate the names of things and to spell them out, even if they're long.
+
+##Attributions:
+* Udemy 'The Complete iOS 9 Developer Course' Guessing Game tutorial by Rob Percival
